be627bc5cc3904fa6980cb927791a1a7350fa0d0
deploy <kurupt_on@proton.me>
2025-06-20 20:37:56 -0300

    Funções minimas implementadas; Não testadas.

--- setup.sh
+++ setup.sh
@@ -15,9 +15,46 @@ install_update() {
 	apt install isc-dhcp-server -y &>/dev/null
 }
 
-menu_select() {
-	read -p ""
+dhcpd_cfg() {
+	cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bkp
+	cat > /etc/dhcp/dhcpd.cof << EOF
+ddns-updates off;
+ddns-update-style none;
+
+subnet $SUBNET netmask $NETMASK {
+	range				"$INIT_RANGE" "$END_RANGE";
+	optionl routers			"$GATEWAY";
+	optinal domain-name-servers	"$DNS";
+}
+
+EOF
+
+}
+
+restart_dhcpd() {
+	echo "Reinicializando o serviço."
+	systemctl restart isc-dhcpd-server.service
+	if [ $? -ne "0" ]; then
+		echo "Erro na reinicialização."
+		exit 1
+	fi
+}
+
+menu_main() {
+	echo "Começando a comfiguração de seu DHCP."
+	echo
+	read -p "subnet :" SUBNET
+	read -p "netmask :" NETMASK
+	read -p "Incio do range :" INIT_RANGE
+	read -p "Fim do range :" END_RANGE
+	read -p "gateway :" GATEWAY
+	read -p "DNS :" DNS
+
+	dhcpd_cfg
+	
 }
 
 test_user
+menu_main
+restart_dhcpd
 
